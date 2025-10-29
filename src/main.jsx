73609fa8faf23d5f68fc5c34da7b4df1e853ab53
Programming Hero <programming.hero1@gmail.com>
2025-10-29 07:03:16 -0500

    added auth context

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { RouterProvider } from "react-router/dom";
 import RootLayout from './layouts/RootLayout.jsx';
 import Home from './components/Home/Home.jsx';
 import AllProducts from './components/AllProducts/AllProducts.jsx';
+import AuthProvider from './contexts/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
@@ -28,6 +29,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
